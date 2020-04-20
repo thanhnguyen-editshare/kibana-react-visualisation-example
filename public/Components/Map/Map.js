8c3bdb086f6685266554e30036f3c5bee25c1ebc
satishwaghela <satishmwaghela@gmail.com>
2020-04-20 18:50:53 +0530

    Custom marker WIP

--- public/Components/Map/Map.js
+++ public/Components/Map/Map.js
@@ -5,6 +5,25 @@ import './topojson';
 
 d3.getEvent = () => require("d3-selection").event;
 
+const features = [{
+  "type": "Feature",
+  "id": "1",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [
+      "55.30472",
+      "25.25817"
+    ],
+    "properties": {
+      "name": "United Arab Emirates",
+      "countryCode": "AE",
+      "value": "1.19"
+    }
+  }
+}];
+
+const j = 0;
+
 export default class Map extends Component {
   componentDidMount () {
     this.renderGraph();
@@ -15,6 +34,7 @@ export default class Map extends Component {
     this.createProjection();
     this.createPath();
     this.renderCountries();
+    this.addMarkers();
   }
 
   createProjection () {
@@ -31,12 +51,13 @@ export default class Map extends Component {
   }
 
   renderCountries () {
-    var g = this.svg.append("g");
+    const g = this.g = this.svg.append("g");
 
-    g.selectAll("path")
+    g.selectAll("path.map-path")
       .data(topojson.object(topology, topology.objects.countries).geometries)
       .enter()
         .append("path")
+        .classed('map-path', true)
         .attr("d", this.path)
         .style("fill","black")
 
@@ -50,9 +71,45 @@ export default class Map extends Component {
     var zoom = d3.behavior.zoom()
       .on("zoom", () => {
         g.attr("transform","translate("+d3.event.translate.join(",")+")scale("+d3.event.scale+")")
+        // this.setMarkerPos();
       });
 
     this.svg.call(zoom)
+  }
+
+  addMarkers () {
+    var vSize = d3.scale.linear()
+      .domain([0,25])
+      .range([0.5,8]);
+
+    var vOp = d3.scale.linear()
+      .domain([0,25])
+      .range([0.25,0.75]);
+
+    var randColour = ['#31558d', '#c52b2d'][Math.floor(Math.random() * 2)];
+
+    var x = this.projection(features[j].geometry.coordinates)[0],
+        y = this.projection(features[j].geometry.coordinates)[1];
+
+    var marker = this.g.append("path")
+      .attr("class", "marker")
+      .attr("d", "M0,0l-8.8-17.7C-12.1-24.3-7.4-32,0-32h0c7.4,0,12.1,7.7,8.8,14.3L0,0z")
+      .attr("transform", "translate(" + x + "," + y + ") scale(0)")
+      .transition()
+      .delay(400)
+      .duration(800)
+      .ease("elastic")
+      .attr("transform", "translate(" + x + "," + y + ") scale(.75)")
+      //.on('mouseover', function(d){})
+      ;
+  }
+
+  setMarkerPos = () => {
+    var x = this.projection(features[j].geometry.coordinates)[0],
+        y = this.projection(features[j].geometry.coordinates)[1];
+
+    this.svg.selectAll('path.marker')
+      .attr("transform", "translate(" + x + "," + y + ") scale(0)")
   }
 
   render () {
